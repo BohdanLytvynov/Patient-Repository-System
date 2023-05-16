2efdd469f49e75a0f56bc104618ef931387d0c48
BohdanLytvynov <blitvinov19@gmail.com>
2023-05-16 23:37:26 +0300

    Initial design of sign in window finished.

---
+++ Models/AuthorizationModels/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.AuthorizationModels
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surename { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Email { get; set; }
+
+        
+
+        public User(Guid id, string name, string surename, string lastname)
+        {
+            Id = id;
+
+            Name = name;
+
+            Surename = surename;
+
+            Lastname = lastname;
+        }
+
+        public User()
+        {
+
+        }
+    }
+}
---
+++ PatientRep/ViewModels/SignInWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ViewModelBaseLib.VM;
+using static DataValidation.Validation;
+
+namespace PatientRep.ViewModels
+{
+    public class SignInWindowViewModel : ViewModelBaseClass
+    {
+        #region Fields
+
+        string m_Login;
+
+        Regex m_email;
+
+        #endregion
+
+        #region Properties
+
+        public string Login { get=> m_Login; set=> Set(ref m_Login, value, nameof(Login)); }
+
+        #endregion
+
+        #region IDataErrorInfo
+        public override string this[string columnName]
+        {
+            get 
+            {
+                string error = String.Empty;
+
+                switch (columnName)
+                {
+                    case nameof(Login):
+
+                        if (String.IsNullOrEmpty(Login))
+                        {
+                            error = "Поле не має бути порожнім!";
+
+                            m_ValidationArray[0] = false;
+
+                            return error;
+                        }
+
+                        if (m_email.IsMatch(Login))// Use email for sign in
+                        {
+                            m_ValidationArray[0] = true;
+                        }
+                        else // Use standart login for sign in
+                        {
+                            m_ValidationArray[0] = true;
+                        }
+
+                        break;
+                }
+
+                return error;
+            }
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Ctor
+        public SignInWindowViewModel()
+        {
+            m_Login = String.Empty;
+
+            m_email = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+
+            m_ValidationArray = new bool[2];
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
