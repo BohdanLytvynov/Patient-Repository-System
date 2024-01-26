97282854dd710915953320e7573f2abe88868a16
BohdanLytvynov <blitvinov19@gmail.com>
2024-01-26 21:32:52 +0200

    update local branch

--- PatientRep/obj/Debug/net6.0-windows/Views/ReportViewer.g.i.cs
+++ PatientRep/obj/Debug/net6.0-windows/Views/ReportViewer.g.i.cs
@@ -62,7 +62,7 @@ namespace PatientRep.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PatientRep;component/views/reportviewer.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PatientRep;V1.0.0.0;component/views/reportviewer.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ReportViewer.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
